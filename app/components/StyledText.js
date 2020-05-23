c335da2c400987f7f28ac85236a1e69908ef3e50
Arsam Sarabi <arsamsarabi@me.com>
2020-05-23 12:13:32 +0100

    🍱 Fonts, icon & logo, test <QuickSandText/>

--- app/components/StyledText.js
+++ app/components/StyledText.js
@@ -1,6 +1,11 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import * as React from 'react'
+import { Text } from 'react-native'
 
 export function MonoText(props) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
+  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />
+}
+
+export function QuickSandText({ style, type = 'body', ...rest }) {
+  const fontFamily = type === 'heading' ? 'quicksand-bold' : 'quicksand'
+  return <Text style={[style, { fontFamily }]} {...rest} />
 }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,9 +1,9 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import * as WebBrowser from 'expo-web-browser'
+import * as React from 'react'
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
-import { MonoText } from '../components/StyledText';
+import { MonoText, QuickSandText } from '../components/StyledText'
 
 export default function HomeScreen() {
   return (
@@ -23,7 +23,9 @@ export default function HomeScreen() {
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
+          <QuickSandText type="heading" style={styles.getStartedText}>
+            Open up the code for this screen:
+          </QuickSandText>
 
           <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
             <MonoText>screens/HomeScreen.js</MonoText>
@@ -49,12 +51,12 @@ export default function HomeScreen() {
         </View>
       </View>
     </View>
-  );
+  )
 }
 
 HomeScreen.navigationOptions = {
   header: null,
-};
+}
 
 function DevelopmentModeNotice() {
   if (__DEV__) {
@@ -62,31 +64,31 @@ function DevelopmentModeNotice() {
       <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
         Learn more
       </Text>
-    );
+    )
 
     return (
       <Text style={styles.developmentModeText}>
         Development mode is enabled: your app will be slower but you can use useful development
         tools. {learnMoreButton}
       </Text>
-    );
+    )
   } else {
     return (
       <Text style={styles.developmentModeText}>
         You are not in development mode: your app will run at full speed.
       </Text>
-    );
+    )
   }
 }
 
 function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
+  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/')
 }
 
 function handleHelpPress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
+    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change',
+  )
 }
 
 const styles = StyleSheet.create({
@@ -176,4 +178,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#2e78b7',
   },
-});
+})
